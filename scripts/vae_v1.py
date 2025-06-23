7faeeff000591a92da5542ee652447cbebb86dd2
Stan <zyyu@umich.edu>
2025-06-22 21:29:46 -0400

    update

--- scripts/vae_v1.py
+++ scripts/vae_v1.py
@@ -1,4 +1,3 @@
-import lightning as L
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
@@ -11,24 +10,41 @@ torch.backends.cudnn.benchmark = False
 class Encoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim):
         super(Encoder, self).__init__()
-        self.FC_input = nn.Linear(input_dim, hidden_dim)
-        self.FC_hidden = nn.Linear(hidden_dim, hidden_dim)
+        self.FC_input1 = nn.Linear(input_dim, hidden_dim)
+        self.FC_hidden1 = nn.Linear(hidden_dim, hidden_dim)
+        self.FC_input2 = nn.Linear(input_dim, hidden_dim)
+        self.FC_hidden2 = nn.Linear(hidden_dim, hidden_dim)
         self.FC_mean = nn.Linear(hidden_dim, latent_dim)
         self.FC_var = nn.Linear(hidden_dim, latent_dim)
         self.activation_fn = nn.LeakyReLU(0.2)
         self.dropout = nn.Dropout(0.1)
         self.batch_norm = nn.BatchNorm1d(hidden_dim, momentum=0.01, eps=0.001)
-        self.layer_norm = nn.LayerNorm(hidden_dim, elementwise_affine=False)
+        self.batch_norm_input = nn.BatchNorm1d(input_dim, momentum=0.01, eps=0.001)
+        self.layer_norm = nn.LayerNorm(hidden_dim, elementwise_affine=True)
 
     def forward(self, x):
-        _h = self.dropout(
-            self.layer_norm(self.batch_norm(self.activation_fn(self.FC_input(x))))
+        _h1 = self.dropout(
+            self.activation_fn(
+                self.layer_norm(
+                    self.batch_norm(self.FC_input1(self.batch_norm_input(x)))
+                )
+            )
         )
-        h = self.dropout(
-            self.layer_norm(self.batch_norm(self.activation_fn(self.FC_hidden(_h))))
+        h1 = self.dropout(
+            self.activation_fn(self.layer_norm(self.batch_norm(self.FC_hidden1(_h1))))
         )
-        mean = self.FC_mean(h)
-        log_var = self.FC_var(h)
+        _h2 = self.dropout(
+            self.activation_fn(
+                self.layer_norm(
+                    self.batch_norm(self.FC_input2(self.batch_norm_input(x)))
+                )
+            )
+        )
+        h2 = self.dropout(
+            self.activation_fn(self.layer_norm(self.batch_norm(self.FC_hidden2(_h2))))
+        )
+        mean = self.FC_mean(h1)
+        log_var = self.FC_var(h2)
 
         return mean, log_var
 
@@ -36,28 +52,38 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, output_dim, hidden_dim, latent_dim):
         super(Decoder, self).__init__()
-        self.FC_latent = nn.Linear(latent_dim, hidden_dim)
-        self.FC_hidden = nn.Linear(hidden_dim, hidden_dim)
-        self.FC_output = nn.Linear(hidden_dim, output_dim)
+        self.FC_latent1 = nn.Linear(latent_dim, hidden_dim)
+        self.FC_hidden1 = nn.Linear(hidden_dim, hidden_dim)
+        self.FC_latent2 = nn.Linear(latent_dim, hidden_dim)
+        self.FC_hidden2 = nn.Linear(hidden_dim, hidden_dim)
+        self.FC_output_mean = nn.Linear(hidden_dim, output_dim)
+        self.FC_output_var = nn.Linear(hidden_dim, output_dim)
+        # self.FC_output_var = nn.Parameter(torch.zeros(output_dim))
         self.activation_fn = nn.LeakyReLU(0.2)
         self.dropout = nn.Dropout(0.1)
         self.batch_norm = nn.BatchNorm1d(hidden_dim, momentum=0.01, eps=0.001)
-        self.layer_norm = nn.LayerNorm(hidden_dim, elementwise_affine=False)
+        self.layer_norm = nn.LayerNorm(hidden_dim, elementwise_affine=True)
 
     def forward(self, x):
-        _h = self.dropout(
-            self.layer_norm(self.batch_norm(self.activation_fn(self.FC_latent(x))))
+        _h1 = self.dropout(
+            self.activation_fn(self.layer_norm(self.batch_norm(self.FC_latent1(x))))
         )
-        h = self.dropout(
-            self.layer_norm(self.batch_norm(self.activation_fn(self.FC_hidden(_h))))
+        h1 = self.dropout(
+            self.activation_fn(self.layer_norm(self.batch_norm(self.FC_hidden1(_h1))))
         )
-        x_hat = self.FC_output(h)
-        log_var_x_hat = self.FC_output(h)
+        _h2 = self.dropout(
+            self.activation_fn(self.layer_norm(self.batch_norm(self.FC_latent2(x))))
+        )
+        h2 = self.dropout(
+            self.activation_fn(self.layer_norm(self.batch_norm(self.FC_hidden2(_h2))))
+        )
+        x_hat = self.FC_output_mean(h1)
+        log_var_x_hat = self.FC_output_var(h2)
 
         return x_hat, log_var_x_hat
 
 
-class VAE(L.LightningDataModule):
+class VAE(pl.LightningModule):
     def __init__(self, Encoder, Decoder):
         super().__init__()
         self.Encoder = Encoder
@@ -95,9 +121,11 @@ class VAE(L.LightningDataModule):
         x = x.view(x.size(0), -1)
         x_hat, log_var_x_hat, mean, log_var = self.forward(x)
         loss = self.loss(x, x_hat, log_var_x_hat, mean, log_var)
+        values = {"loss": loss}  # add more items if needed
+        self.log_dict(values, prog_bar=True)
         return loss
 
-    def configure_optimizes(self):
+    def configure_optimizers(self):  # Fixed typo from configure_optimizes
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 

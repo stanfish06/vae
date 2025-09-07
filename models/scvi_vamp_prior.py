bb31f3200a567b7702e7707bf759a5f946358355
Stan <zyyu@umich.edu>
2025-09-07 12:14:14 -0400

    update: custom scvi model

---
+++ models/scvi_vamp_prior.py
@@ -0,0 +1,51 @@
+from anndata import AnnData
+from scvi import REGISTRY_KEYS
+from scvi.data import AnnDataManager
+from scvi.data.fields import (
+    CategoricalJointObsField,
+    CategoricalObsField,
+    LayerField,
+    NumericalJointObsField,
+    NumericalObsField,
+)
+from scvi.model.base import BaseModelClass, UnsupervisedTrainingMixin, VAEMixin
+from scvi.module import VAE
+
+
+class SCVI(UnsupervisedTrainingMixin, BaseModelClass, VAEMixin):
+    def __init__(self, adata, n_latent: int = 30, **model_kwargs):
+        super().__init__(adata)
+        self.module = VAE(
+            n_input=self.summary_stats["n_vars"],
+            n_batch=self.summary_stats["n_batch"],
+            n_latent=n_latent,
+            **model_kwargs,
+        )
+        self._model_summary_string = (
+            f"SCVI model with the following parameters: \nn_latent {n_latent}"
+        )
+        self.init_params_ = self._get_init_params(locals())
+
+    @classmethod
+    def setup_anndata(
+        cls,
+        adata: AnnData,
+        batch_key: str | None = None,
+        layer: str | None = None,
+        **kwargs,
+    ) -> AnnData | None:
+        setup_method_args = cls._get_setup_method_args(**locals())
+        anndata_fields = [
+            LayerField(REGISTRY_KEYS.X_KEY, layer, is_count_data=True),
+            CategoricalObsField(REGISTRY_KEYS.BATCH_KEY, batch_key),
+            # Dummy fields required for VAE class.
+            CategoricalObsField(REGISTRY_KEYS.LABELS_KEY, None),
+            NumericalObsField(REGISTRY_KEYS.SIZE_FACTOR_KEY, None, required=False),
+            CategoricalJointObsField(REGISTRY_KEYS.CAT_COVS_KEY, None),
+            NumericalJointObsField(REGISTRY_KEYS.CONT_COVS_KEY, None),
+        ]
+        adata_manager = AnnDataManager(
+            fields=anndata_fields, setup_method_args=setup_method_args
+        )
+        adata_manager.register_fields(adata, **kwargs)
+        cls.register_manager(adata_manager)
---
+++ models/vae_v5.py
@@ -0,0 +1,378 @@
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from torch.distributions import Independent, NegativeBinomial
+
+pl.seed_everything(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, condition_dim, hidden_dim, latent_dim):
+        super(Encoder, self).__init__()
+        self.input_dim = input_dim
+        self.condition_dim = condition_dim
+        self.latent_dim = latent_dim
+        self.FC_input = nn.Linear(input_dim + condition_dim, hidden_dim)
+        self.FC_hidden = nn.Linear(hidden_dim + condition_dim, hidden_dim)
+        self.FC_mean = nn.Linear(hidden_dim, latent_dim)
+        self.FC_var = nn.Linear(hidden_dim, latent_dim)
+        self.activation_fn = nn.LeakyReLU(0.2)
+        self.dropout = nn.Dropout(0.1)
+        self.batch_norm = nn.BatchNorm1d(hidden_dim, momentum=0.01, eps=0.001)
+        self.batch_norm_input = nn.BatchNorm1d(input_dim, momentum=0.01, eps=0.001)
+
+    def forward(self, x, c):
+        # x_norm = self.batch_norm_input(x)
+        x_norm = self.batch_norm_input(x)
+        _h = self.dropout(
+            self.activation_fn(
+                self.batch_norm(self.FC_input(torch.concat([x_norm, c], dim=1)))
+            )
+        )
+        h = self.dropout(
+            self.activation_fn(
+                self.batch_norm(self.FC_hidden(torch.concat([_h, c], dim=1)))
+            )
+        )
+        mean = self.FC_mean(h)
+        log_var = self.FC_var(h)
+
+        return mean, log_var
+
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, condition_dim, hidden_dim, latent_dim):
+        super(Decoder, self).__init__()
+        self.latent_dim = latent_dim
+        self.FC_latent = nn.Linear(latent_dim + condition_dim, hidden_dim)
+        self.FC_hidden = nn.Linear(hidden_dim + condition_dim, hidden_dim)
+        self.FC_output_mean = nn.Linear(hidden_dim, output_dim)
+        self.log_theta = nn.Parameter(torch.zeros(output_dim))
+        self.activation_fn = nn.LeakyReLU(0.2)
+        self.dropout = nn.Dropout(0.1)
+        self.batch_norm = nn.BatchNorm1d(hidden_dim, momentum=0.01, eps=0.001)
+
+    def forward(self, x, c):
+        _h = self.dropout(
+            self.activation_fn(
+                self.batch_norm(self.FC_latent(torch.concat([x, c], dim=1)))
+            )
+        )
+        h = self.dropout(
+            self.activation_fn(
+                self.batch_norm(self.FC_hidden(torch.concat([_h, c], dim=1)))
+            )
+        )
+        log_x_hat = self.FC_output_mean(h)
+        log_theta = self.log_theta.expand(log_x_hat.size(0), -1)
+
+        return log_x_hat, log_theta
+
+
+class HVAE(pl.LightningModule):
+    def __init__(
+        self,
+        Encoder,
+        Decoder,
+        nLeapFrog,
+        InitLeapFrogStep,
+        MaxLeapFrogStep,
+        lr,
+        beta,
+        init_alpha,
+        GraphRegWeight,
+        MMD,
+        MMDdim,
+        MMDgamma,
+    ):
+        super().__init__()
+        self.Encoder = Encoder
+        self.Decoder = Decoder
+        self.nLeapFrog = nLeapFrog
+        self.InitLeapFrogStep = InitLeapFrogStep
+        self.MaxLeapFrogStep = MaxLeapFrogStep
+        self.lr = lr
+        self.beta = beta
+        self.init_alpha = init_alpha
+        self.GraphRegWeight = GraphRegWeight
+        self.MMD = MMD
+        self.MMDdim = MMDdim
+        self.MMDgamma = MMDgamma
+
+        init_alphas = self.init_alpha * np.ones(self.nLeapFrog)
+        init_alphas_reparam = np.log(init_alphas / (1 - init_alphas))
+        self.alphas_reparam = nn.Parameter(
+            torch.from_numpy(init_alphas_reparam.astype(np.float32))
+        )
+
+        # learnable leapfrog step size
+        init_lf = self.InitLeapFrogStep * np.ones(self.Encoder.latent_dim)
+        init_lf_reparam = np.log(
+            init_lf / (self.MaxLeapFrogStep - self.InitLeapFrogStep)
+        )
+        init_lf_reparam = np.tile(init_lf_reparam, (self.nLeapFrog, 1))
+
+        self.lf_reparam = nn.Parameter(
+            torch.from_numpy(init_lf_reparam.astype(np.float32))
+        )
+
+    def _his(self, z_0, p_0, x, c, lf_eps):
+        _z = z_0
+        _p = p_0
+        for k in range(self.nLeapFrog):
+            p_half = _p - 1.0 / 2 * lf_eps[k, :] * self._dU_dz(_z, x, c)
+            _z = _z + lf_eps[k, :] * p_half
+            _p = p_half - 1.0 / 2 * lf_eps[k, :] * self._dU_dz(_z, x, c)
+        return _z, _p
+
+    def _dU_dz(self, z, x, c):
+        z = z.requires_grad_(True)
+
+        log_x_hat, log_theta = self.Decoder(z, c)
+        theta = torch.exp(log_theta)
+        # Clamp for numerical stability
+        log_x_hat = torch.clamp(log_x_hat, max=1e8)
+        theta = torch.clamp(theta, min=1e-8, max=1e8)
+        log_theta = torch.clamp(log_theta, max=1e8)
+        # Parameterization for Negative Binomial
+        # total_count = theta, probs = theta / (theta + x_hat)
+        pxz = Independent(
+            NegativeBinomial(total_count=theta, logits=log_x_hat - log_theta), 1
+        )
+        reconst_NLL = -pxz.log_prob(x)
+        prior_NLL = 0.5 * torch.sum(z**2, dim=-1)
+
+        U = (reconst_NLL + prior_NLL).sum()
+        return torch.autograd.grad(U, z, create_graph=True)[0]
+
+    def reparameterization(self, mean, sd):
+        epsilon = torch.randn_like(sd)
+        z = mean + sd * epsilon
+        return z
+
+    def forward(self, x, c):
+        # Calculate alphas and T_0
+        alphas = torch.sigmoid(self.alphas_reparam)
+        T_0 = torch.prod(alphas) ** (-2)
+
+        # Calculate leapfrog steps
+        lf_eps = self.MaxLeapFrogStep * torch.sigmoid(self.lf_reparam)
+
+        mean_z, log_var_z = self.Encoder(x, c)
+        z_0 = self.reparameterization(mean_z, torch.exp(0.5 * log_var_z))
+        p_0 = torch.sqrt(T_0) * torch.randn_like(z_0)
+        (z_k, p_k) = self._his(z_0, p_0, x, c, lf_eps)
+        log_x_hat, log_theta = self.Decoder(z_k, c)
+        return (log_x_hat, log_theta, mean_z, log_var_z, z_k, p_k)
+
+    def get_latent_representation(self, x, c, K=10):
+        z_samples = []
+        # Calculate alphas and T_0
+        alphas = torch.sigmoid(self.alphas_reparam)
+        T_0 = torch.prod(alphas) ** (-2)
+        # Calculate leapfrog steps
+        lf_eps = self.MaxLeapFrogStep * torch.sigmoid(self.lf_reparam)
+        mean_z, log_var_z = self.Encoder(x, c)
+        for _ in range(K):
+            z_0 = self.reparameterization(mean_z, torch.exp(0.5 * log_var_z))
+            p_0 = torch.sqrt(T_0) * torch.randn_like(z_0)
+            z_k, _ = self._his(z_0, p_0, x, c, lf_eps)
+            z_samples.append(z_k.unsqueeze(0))
+        z_stack = torch.cat(z_samples, dim=0)
+        z_median = z_stack.median(dim=0).values
+        return z_median
+
+    # ELBO
+    def loss(
+        self,
+        x,
+        c,
+        log_x_hat,
+        log_theta,
+        mean_z,
+        log_var_z,
+        z_k,
+        p_k,
+        obs_idx,
+        obs_nei_idx,
+        obs_nei_conn,
+    ):
+        # reconstruction NLL
+        theta = torch.exp(log_theta)
+        # Clamp for numerical stability
+        log_x_hat = torch.clamp(log_x_hat, max=1e8)
+        theta = torch.clamp(theta, min=1e-8, max=1e8)
+        log_theta = torch.clamp(log_theta, max=1e8)
+        # Parameterization for Negative Binomial
+        # total_count = theta, probs = theta / (theta + x_hat)
+        pxz = Independent(
+            NegativeBinomial(total_count=theta, logits=log_x_hat - log_theta), 1
+        )
+        NLL_x = -pxz.log_prob(x)  # shape: (batch,)
+
+        # negative KL term between q(z|x) and p(z)
+        latent_entropy_posterior = torch.sum(0.5 * log_var_z, dim=-1)
+        NLL_z_prior = -0.5 * torch.sum(z_k * z_k, dim=-1)
+        NLL_p_prior = -0.5 * torch.sum(p_k * p_k, dim=-1)
+
+        # graph regularization
+        gLoss = torch.tensor(0.0, device=z_k.device, dtype=z_k.dtype)
+        if self.GraphRegWeight > 0:
+            # Create a mapping from global dataset index to local batch index
+            global_to_local_idx = {
+                int(global_idx): local_idx
+                for local_idx, global_idx in enumerate(obs_idx)
+            }
+            n_pairs_in_batch = 0
+            for i_local, i_global in enumerate(obs_idx):
+                i_global_int = int(i_global)
+                for j_global in obs_nei_idx[global_to_local_idx[i_global_int]]:
+                    # Check if the neighbor is also in the current batch
+                    # if j_global is -1, then obs i misses jth neghbor, so skip
+                    if j_global < 0:
+                        continue
+                    if j_global in global_to_local_idx:
+                        n_pairs_in_batch += 1
+                        j_local = global_to_local_idx[j_global]
+                        weight = obs_nei_conn[i_global_int, j_global]
+                        gLoss += weight * torch.sum((z_k[i_local] - z_k[j_local]) ** 2)
+            if n_pairs_in_batch > 0:
+                gLoss = gLoss / n_pairs_in_batch
+            gLoss = self.GraphRegWeight * gLoss
+
+        # MMD regularization
+        MMDLoss = torch.tensor(0.0, device=z_k.device, dtype=z_k.dtype)
+        if self.MMD > 0:
+            W = torch.randn(
+                z_k.size(1), self.MMDdim, device=x.device
+            )  # [latent_dim, D]
+            b = torch.rand(1, self.MMDdim, device=x.device) * 2 * torch.pi  # [1, D]
+            scale = torch.sqrt(torch.tensor(2.0 / self.MMDgamma, device=x.device))
+            norm_factor = torch.sqrt(torch.tensor(2.0 / self.MMDdim, device=x.device))
+
+            phi_all = norm_factor * torch.cos(scale * (z_k @ W) + b)  # [batch_size, D]
+            phi_mean = phi_all.mean(dim=0)  # [D]
+
+            for k in range(c.size(1)):
+                mask = c[:, k].bool()
+                if mask.sum() > 1:
+                    phi_k = phi_all[mask]  # [n_k, D]
+                    phi_k_mean = phi_k.mean(dim=0)  # [D]
+                    MMDLoss += self.MMD * torch.sum((phi_k_mean - phi_mean) ** 2)
+
+        return (
+            (
+                NLL_x
+                - self.beta
+                * (
+                    latent_entropy_posterior
+                    + NLL_z_prior
+                    + NLL_p_prior
+                    + self.Encoder.latent_dim
+                )
+            ).mean()
+            + gLoss
+            + MMDLoss
+        )
+
+    def training_step(self, batch, batch_idx):
+        x, c, obs_idx, obs_nei_idx, obs_nei_conn = batch
+        log_x_hat, log_theta, mean_z, log_var_z, z_k, p_k = self.forward(x, c)
+        loss = self.loss(
+            x=x,
+            c=c,
+            log_x_hat=log_x_hat,
+            log_theta=log_theta,
+            mean_z=mean_z,
+            log_var_z=log_var_z,
+            z_k=z_k,
+            p_k=p_k,
+            obs_idx=obs_idx,
+            obs_nei_idx=obs_nei_idx,
+            obs_nei_conn=obs_nei_conn,
+        )
+        values = {"loss": loss}
+        self.log_dict(values, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+
+if __name__ == "__main__":
+    input_dim = 10
+    condition_dim = 2
+    hidden_dim = 20
+    latent_dim = 5
+    output_dim = 10
+    n_obs = 10
+
+    np.random.seed(42)
+    x = torch.from_numpy(
+        np.random.randint(0, 100, size=(n_obs, input_dim)).astype(np.float32)
+    )
+    np.random.seed(88)
+    c = np.random.binomial(n=1, p=0.4, size=n_obs)
+    c = torch.from_numpy(np.vstack([c, np.logical_not(c)]).T.astype(np.float32))
+
+    # Dummy graph data
+    obs_idx = torch.arange(n_obs)
+    obs_nei_idx = {i: [] for i in range(n_obs)}
+    obs_nei_conn = {}
+    for i in range(n_obs - 1):
+        # Connect node i and i+1
+        obs_nei_idx[i].append(i + 1)
+        obs_nei_idx[i + 1].append(i)
+        # Add weights
+        obs_nei_conn[i, i + 1] = 1.0
+        obs_nei_conn[i + 1, i] = 1.0
+
+    m = HVAE(
+        Encoder=Encoder(
+            input_dim=input_dim,
+            condition_dim=condition_dim,
+            hidden_dim=hidden_dim,
+            latent_dim=latent_dim,
+        ),
+        Decoder=Decoder(
+            output_dim=output_dim,
+            condition_dim=condition_dim,
+            hidden_dim=hidden_dim,
+            latent_dim=latent_dim,
+        ),
+        nLeapFrog=10,
+        InitLeapFrogStep=0.01,
+        MaxLeapFrogStep=0.1,
+        lr=1e-4,
+        beta=1,
+        init_alpha=0.9,
+        GraphRegWeight=0.1,
+        MMD=0.1,
+        MMDdim=50,
+        MMDgamma=1.0,
+    )
+    # test potential gradient
+    np.random.seed(1)
+    log_x_hat, log_theta, mean_z, log_var_z, z_k, p_k = m.forward(x, c)
+    loss = m.loss(
+        x=x,
+        c=c,
+        log_x_hat=log_x_hat,
+        log_theta=log_theta,
+        mean_z=mean_z,
+        log_var_z=log_var_z,
+        z_k=z_k,
+        p_k=p_k,
+        obs_idx=obs_idx,
+        obs_nei_idx=obs_nei_idx,
+        obs_nei_conn=obs_nei_conn,
+    )
+    print(f"loss : {loss}")
+    z_0 = m.reparameterization(mean_z, torch.exp(0.5 * log_var_z))
+    print(m._dU_dz(z_0, x, c))
+    print(m.forward(x, c))
+    print(m.get_latent_representation(x, c, K=10))

f646fa99cb23caf86fd12fa9f16006922b31a06c
Stan <zyyu@umich.edu>
2025-09-08 01:50:31 -0400

    update: sysvi

--- models/scvi_vamp_prior.py
+++ models/scvi_vamp_prior.py
@@ -9,18 +9,37 @@ from scvi.data.fields import (
     NumericalObsField,
 )
 from scvi.model.base import BaseModelClass, UnsupervisedTrainingMixin, VAEMixin
-from scvi.module import VAE
+from scvi.module import VAE, VAEC
 
 
 class SCVI(UnsupervisedTrainingMixin, BaseModelClass, VAEMixin):
-    def __init__(self, adata, n_latent: int = 30, **model_kwargs):
+    def __init__(
+        self,
+        adata: AnnData,
+        n_hidden: int = 256,
+        n_layers: int = 1,
+        n_latent: int = 30,
+        use_vamp_prior: bool = True,
+        **model_kwargs,
+    ):
         super().__init__(adata)
-        self.module = VAE(
-            n_input=self.summary_stats["n_vars"],
-            n_batch=self.summary_stats["n_batch"],
-            n_latent=n_latent,
-            **model_kwargs,
-        )
+        if use_vamp_prior:
+            self.module = VAEC(
+                n_input=self.summary_stats.n_vars,
+                n_batch=getattr(self.summary_stats, "n_batch", 0),
+                n_labels=self.summary_stats.n_labels,
+                n_hidden=n_hidden,
+                n_latent=n_latent,
+                n_layers=n_layers,
+                **model_kwargs,
+            )
+        else:
+            self.module = VAE(
+                n_input=self.summary_stats["n_vars"],
+                n_batch=self.summary_stats["n_batch"],
+                n_latent=n_latent,
+                **model_kwargs,
+            )
         self._model_summary_string = (
             f"SCVI model with the following parameters: \nn_latent {n_latent}"
         )
